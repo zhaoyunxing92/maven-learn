2b5607ef5265c716d06de107d20301521c5a1ed4
zhaoyunxing92 <2385585770@qq.com>
2019-08-27 14:38:42 +0800

    [add]基本骨架完成

---
+++ readme.md
@@ -0,0 +1,10 @@
+# maven learn
+
+[![Build Status](https://travis-ci.com/zhaoyunxing92/maven-learn.svg?branch=master)](https://travis-ci.com/zhaoyunxing92/maven-learn)
+
+主要解决在使用maven中遇到的盲点以及怎么使用maven架构一个微服务
+
+## 目录结构说明
+
+
+
---
+++ server/order-server/order-api/src/main/java/io/github/sunny/order/entities/Order.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright(C) 2019 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package io.github.sunny.order.entities;
+
+/**
+ * @author zhaoyunxing
+ * @date: 2019-08-27 14:37
+ * @des:
+ */
+
+public class Order {
+}
