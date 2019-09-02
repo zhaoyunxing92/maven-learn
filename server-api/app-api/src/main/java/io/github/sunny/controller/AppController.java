c126afb4a0b677e88d271160b494f06c569dc523
zhaoyunxing92 <2385585770@qq.com>
2019-09-02 10:46:57 +0800

    [up]

--- server-api/app-api/src/main/java/io/github/sunny/controller/AppController.java
+++ server-api/app-api/src/main/java/io/github/sunny/controller/AppController.java
@@ -3,17 +3,14 @@
  */
 package io.github.sunny.controller;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import io.github.sunny.core.msg.Response;
 import io.github.sunny.order.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
