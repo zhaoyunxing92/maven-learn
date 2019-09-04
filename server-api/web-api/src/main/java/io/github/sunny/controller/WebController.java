bbd99c92fa3c882001f704610fb48e40678ddef4
zhaoyunxing92 <2385585770@qq.com>
2019-09-04 18:25:43 +0800

    [up]更新maven插件使用

---
+++ server-api/web-api/src/main/java/io/github/sunny/controller/WebController.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright(C) 2019 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package io.github.sunny.controller;
+
+import io.github.sunny.core.msg.Response;
+import io.github.sunny.order.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author zhaoyunxing
+ * @date: 2019-08-27 17:16
+ * @des:
+ */
+@RestController
+@Slf4j
+public class WebController {
+
+    private final OrderService orderService;
+
+    @Autowired
+    public WebController(OrderService orderService) {this.orderService = orderService;}
+
+    @GetMapping("/order")
+    public Response getOrder() {
+        return orderService.findById("2");
+    }
+}
