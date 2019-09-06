4a39510b2e5bf6192174187011534e89956f12f9
zhaoyunxing92 <2385585770@qq.com>
2019-09-06 16:29:31 +0800

    [up]更新文档

--- readme.md
+++ readme.md
@@ -36,6 +36,37 @@
 
 ### 动态日志级别
 
-这个根据项目架构来，如果项目中集成了[spring-boot-starter-actuator](https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#production-ready)则用`actuator`如果没有自己编写
+这个根据项目架构来，如果项目中集成了[spring-boot-starter-actuator](https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#production-ready)则用`actuator`，如果没有自己编写
 
 #### [spring-boot-starter-actuator](https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#production-ready)
+
+```shell
+# 通过 http://localhost:8081/log 观察日志输出
+curl http://localhost:8081/actuator/loggers/io.github.sunny.controller -X POST -H "Content-Type:application/json" -d '{"configuredLevel": "debug"}'
+```
+
+#### 编码方式修改
+
+```java
+    /**
+     * 动态修改日志级别
+     *
+     * @param pgk   包路径 eg:com.io.github.sunny
+     * @param level 日志级别 ALL、TRACE、DEBUG、INFO、WARN、ERROR
+     * @return {@link Response}
+     */
+    @PostMapping("/log/{pgk}/{level}")
+    public Response setLogLevel(@PathVariable("pgk") String pgk, @PathVariable("level") String level) {
+
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        loggerContext.getLogger(pgk).setLevel(Level.valueOf(level));
+        
+        return Response.createSuccess("成功修改包[%s]日志级别为：[%s]", null, pgk, level);
+    }
+```
+
+通过curl修改或者postman修改
+
+```shell
+curl http://localhost:8081/log/io.github.sunny.controller/info -X POST
+```
--- server-api/app-api/src/main/java/io/github/sunny/controller/AppController.java
+++ server-api/app-api/src/main/java/io/github/sunny/controller/AppController.java
@@ -3,15 +3,19 @@
  */
 package io.github.sunny.controller;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
 import com.alibaba.fastjson.JSONObject;
 import io.github.sunny.core.msg.Response;
 import io.github.sunny.order.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
@@ -48,6 +52,22 @@ public class AppController {
             return Response.createError("加载[git.properties]属性文件异常,请使用【java -jar】方式启动项目", 404);
         }
 
+    }
+
+    /**
+     * 动态修改日志级别
+     *
+     * @param pgk   包路径 eg:com.io.github.sunny
+     * @param level 日志级别 ALL、TRACE、DEBUG、INFO、WARN、ERROR
+     * @return {@link Response}
+     */
+    @PostMapping("/log/{pgk}/{level}")
+    public Response setLogLevel(@PathVariable("pgk") String pgk, @PathVariable("level") String level) {
+
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        loggerContext.getLogger(pgk).setLevel(Level.valueOf(level));
+
+        return Response.createSuccess("成功修改包[%s]日志级别为：[%s]", null, pgk, level);
     }
 
     @GetMapping("/log")
